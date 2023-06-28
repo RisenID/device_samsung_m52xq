2011b1b9efcfddbad118297d2d259b53594ecf22
Ruchit <ruchitmarathe@gmail.com>
2023-06-28 19:47:57 +0530

    m52sxq: Bringup evox

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m52xq.mk
+    $(LOCAL_DIR)/evolution_m52xq.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_m52xq-userdebug \
-    lineage_m52xq-user \
-    lineage_m52xq-eng
+    evolution_m52xq-userdebug \
+    evolution_m52xq-user \
+    evolution_m52xq-eng
